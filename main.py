c77c8c993b51f0daa2fa59e6fc76ac322ea061c6
xpcosmos <mikeias.d.s.o@gmail.com>
2023-01-30 20:57:34 -0300

    API construida com 1 parâmetro

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import pandas as pd
 
-df = pd.read_csv('/Users/mikeiasoliveira/Documents/Projetos/meu-primeiro-MLOps/data/data.csv')
-X = df.tamanho
-y = df.preco
 SEED = 42
+
+df = pd.read_csv('/Users/mikeiasoliveira/Documents/Projetos/meu-primeiro-MLOps/data/data.csv')
+
+X = df[['tamanho']]
+y = df.preco
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state= SEED, test_size=0.3)
+
 model = LinearRegression()
 model.fit(X_train, y_train)
 
@@ -18,11 +22,18 @@ app = Flask(__name__)
 def home():
     return "Minha primeira API."
 
+@app.route('/cotacao/<int:tamanho>')
+def cotacao(tamanho):
+    preco = model.predict([[tamanho]])
+    return str(preco)
+
+
 @app.route('/sentimento/<frase>')
 def sentimento(frase):
     tb = TextBlob(frase)
     tb_en = tb.translate(from_lang='pt', to= 'en')
     polaridade = tb_en.sentiment.polarity
     return f"Polaridade: {polaridade}"
+
     
 app.run(debug=True)
