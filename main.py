7b22429924090341213340d18105f4d356642050
xpcosmos <mikeias.d.s.o@gmail.com>
2023-01-30 19:58:47 -0300

    Criação de primeiro endpoint

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from textblob import TextBlob
 
-app = Flask('meu_app')
+
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Minha primeira API."
 
-app.run()
+@app.route('/sentimento/<frase>')
+def sentimento(frase):
+    tb = TextBlob(frase)
+    tb_en = tb.translate(from_lang='pt', to= 'en')
+    polaridade = tb_en.sentiment.polarity
+    return f"Polaridade: {polaridade}"
+    
+app.run(debug=True)
