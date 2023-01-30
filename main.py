60e13ea8eadad71d193ef697dcde86eac3d332ec
xpcosmos <mikeias.d.s.o@gmail.com>
2023-01-30 20:45:34 -0300

    Criação de modelo de regressão

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from flask import Flask
 from textblob import TextBlob
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import pandas as pd
 
+df = pd.read_csv('/Users/mikeiasoliveira/Documents/Projetos/meu-primeiro-MLOps/data/data.csv')
+X = df.tamanho
+y = df.preco
+SEED = 42
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state= SEED, test_size=0.3)
+model = LinearRegression()
+model.fit(X_train, y_train)
 
 app = Flask(__name__)
 
